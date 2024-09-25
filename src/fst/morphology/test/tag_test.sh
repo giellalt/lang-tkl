2107b2266d7af1a74b47a57b7722a4b6f0a245ee
Trond Trosterud <trond.trosterud@uit.no>
2024-09-25 09:26:47 +0200

    Generate project

---
+++ src/fst/morphology/test/tag_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ensure that we are ran from make or setup properly
+if test -z "$srcdir" ; then
+    echo "srcdir= not set, this must be run from make or set srcdir=."
+    exit 2
+fi
+if test -z "$GIELLA_CORE" ; then
+    echo "GIELLA_CORE= must point to giella-core"
+    exit 2
+fi
+relpath="$GIELLA_CORE/scripts/"
+testrunner="$relpath/tag_test.sh"
+if ! test -x "$testrunner" ; then
+    echo "missing test runner in $testrunner"
+    exit 77
+fi
+lexc=$srcdir/../lexicon.lexc
+if ! test -f $lexc ; then
+    echo combined $lexc missing or disappeared
+    exit 1
+fi
+$testrunner $lexc
